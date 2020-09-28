49ff53c8fcf6259f8cbb49e6bcf8f3396fdb313b
Megan Parsons <meganparsons@alumni.harvard.edu>
2020-09-27 20:10:03 -0400

    Eliminate Tweet truncations

--- tweepy_test.py
+++ tweepy_test.py
@@ -22,12 +22,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 # Get the User object
-id='SWE_grad'
+id='MeganMParsons'
 user = api.get_user(id)
 print('Screen Name: ', user.screen_name)
 print('Followers Count: ', user.followers_count)
-# for friend in user.friends():
-#   print(friend.screen_name)
+for friend in user.friends():
+  print(friend.screen_name)
 
 # Pull home timeline associated with personal account @MeganMParsons
 public_tweets = api.home_timeline(tweet_mode='extended')
@@ -48,3 +48,20 @@ for status in tweepy.Cursor(api.user_timeline, id=id, tweet_mode='extended').ite
     except AttributeError:
       print(status.full_text, '\n')
 
+# Search for specific query and return results
+query = '#COVID-19'
+max_tweets = 10
+result_type = 'recent'
+lang = 'en'
+tweet_mode = 'extended'
+for status in tweepy.Cursor(api.search, q=query, count=max_tweets, lang=lang, result_type=result_type, tweet_mode=tweet_mode).items(max_tweets):
+  if hasattr(status, 'retweeted_status'):   # Check if retweet
+    try:
+      print(status.retweeted_status.text, '\n')
+    except AttributeError:
+      print(status.retweeted_status.full_text, '\n')
+  else:
+    try:
+      print(status.extended_tweet.full_text, '\n')
+    except AttributeError:
+      print(status.full_text, '\n')
