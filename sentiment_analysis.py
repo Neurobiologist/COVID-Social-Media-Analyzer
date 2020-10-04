481284d4efd8bafce65089f720329b370ff4a1c8
Megan Parsons <meganparsons@alumni.harvard.edu>
2020-10-04 19:59:35 -0400

    Add preprocess step

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-NLP Test
+Sentiment Analysis Project
 
 @author: MeganParsons
 """
@@ -10,7 +10,6 @@ NLP Test
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-client = language.LanguageServiceClient()
 # Import Tweepy
 import tweepy
 # General Imports
@@ -22,9 +21,45 @@ api_secret = os.environ.get("TWITTER_API_SECRET")
 access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-# Authentication
+# Tweepy Authentication
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-# Access the API
+# Access the Tweepy API
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+# Access the Google NLP API
+client = language.LanguageServiceClient()
+
+def preprocess_tweet(status):
+  # Search for specific query and return results
+  if hasattr(status, 'retweeted_status'):   # Check if retweet
+    try:
+      status = status.retweeted_status.text
+    except AttributeError:
+      status = status.retweeted_status.full_text
+  else:
+    try:
+      status = status.extended_tweet.full_text
+    except AttributeError:
+      status = status.full_text
+
+  return status
+
+def main():
+  
+  query = '#COVID-19'
+  max_tweets = 2
+  result_type = 'recent'
+  lang = 'en'
+  tweet_mode = 'extended'
+
+  for status in tweepy.Cursor(api.search, q=query, count=max_tweets, lang=lang, result_type=result_type, tweet_mode=tweet_mode).items(max_tweets):
+    status = preprocess_tweet(status)
+    print(status)
+
+
+  # Return Tweets of interest
+
+
+if __name__ == "__main__":
+  main()
