dcde8c5be1dc81bd103fea5b5e52d7e32c177c16
Megan Parsons <meganparsons@alumni.harvard.edu>
2020-10-05 00:29:45 -0400

    Add dataframe

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -14,6 +14,12 @@ from google.cloud.language import types
 import tweepy
 # General Imports
 import os
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+# Pandas Settings
+pd.set_option('max_colwidth', 280)  # Capture full tweet
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 # Fetch Twitter API key and access token from environment variables
 api_key = os.environ.get("TWITTER_API_KEY")
@@ -31,7 +37,7 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 client = language.LanguageServiceClient()
 
 def preprocess_tweet(status):
-  # Search for specific query and return results
+  # Return full text of tweet
   if hasattr(status, 'retweeted_status'):   # Check if retweet
     try:
       status = status.retweeted_status.text
@@ -45,20 +51,79 @@ def preprocess_tweet(status):
 
   return status
 
+def sentiment_analysis(tweet):
+  # Sentiment analysis on input
+  document = language.types.Document(
+  content=tweet, 
+  type=enums.Document.Type.PLAIN_TEXT)
+
+  response = client.analyze_sentiment(
+    document=document,
+    encoding_type='UTF32',
+    )
+
+  sentiment = response.document_sentiment
+
+  return sentiment
+
+
+
 def main():
+
+  # Create dataframe
+  tweet_data = pd.DataFrame()
+  date = []
+  content = []
+  sentiment_score = []
+  sentiment_mag = []
+  i=0
+
+  # Twitter Handles of Interest
+  realDonaldTrump = '25073877'
+  CDCgov = '146569971'
   
-  query = '#COVID-19'
-  max_tweets = 2
+  # Search Parameters
+  query = 'from:CDCgov'
+  max_tweets = 10
   result_type = 'recent'
   lang = 'en'
   tweet_mode = 'extended'
-
-  for status in tweepy.Cursor(api.search, q=query, count=max_tweets, lang=lang, result_type=result_type, tweet_mode=tweet_mode).items(max_tweets):
-    status = preprocess_tweet(status)
-    print(status)
+  since_id = '2020-02-01'
+  counter = 0
 
 
-  # Return Tweets of interest
+  # for status in tweepy.Cursor(api.user_timeline, id=realDonaldTrump, since_id=since_id, tweet_mode='extended').items():
+  #   tweet = preprocess_tweet(status)
+  #   counter += 1
+  #   print(counter)
+  #   if any(keyword in tweet for keyword in ('COVID', 'covid', 'China virus')):
+  #     sentiment = sentiment_analysis(tweet)
+  #     print(status.created_at, '\t', tweet, '\nSentiment:', sentiment.score, sentiment.magnitude, '\n\n')
+
+
+  #   if counter == max_tweets:
+  #     break
+
+  for status in tweepy.Cursor(api.search, q=query, count=max_tweets, lang=lang, result_type=result_type, tweet_mode=tweet_mode).items():
+    tweet = preprocess_tweet(status)
+    counter += 1
+    print(counter)
+    if any(keyword in tweet for keyword in ('COVID', 'covid', 'China virus')):
+      sentiment = sentiment_analysis(tweet)
+      print(tweet, sentiment.score)
+      #Store values
+      df = pd.DataFrame({'Date':[status.created_at],
+        'Tweet':tweet,
+        'Sentiment_Score':[sentiment.score],
+        'Sentiment_Magnitude':[sentiment.magnitude]})
+      print(df)
+      tweet_data = tweet_data.append(df, ignore_index=True)
+      print(tweet_data)
+
+    if counter == max_tweets:
+     break
+
+  print(tweet_data)
 
 
 if __name__ == "__main__":
